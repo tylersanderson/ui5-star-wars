df6e89729e27905ff1c2c8ca26ca329693d9587b
tylersanderson <tylersanderson@hotmail.com>
2020-09-27 11:30:41 -0500

    built out people table with back/next controls and async data flow

--- src/pages/people.component.jsx
+++ src/pages/people.component.jsx
@@ -14,6 +14,7 @@ import {
   FlexBoxDirection,
   AnalyticalTable,
   Icon,
+  Button,
 } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
 import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
@@ -28,40 +29,60 @@ import "@ui5/webcomponents-icons/dist/Assets.js"; // Only if using the @ui5/webc
 
 export function People() {
   const [peopleList, setPeopleList] = useState([]);
-  const [peopleListSchema, setPeopleListSchema] = useState([]);
   const [peopleTableColumnHeaders, setPeopleTableColumnHeaders] = useState([]);
-  useEffect(() => {
-    async function fetchPeopleList() {
-      const peopleResult = await fetch(
-        "https://swapi.dev/api/people/"
-      ).then((response) => response.json());
-      setPeopleList(peopleResult);
-    }
-    async function fetchPeopleListSchema() {
-      const peopleSchemaResult = await fetch(
-        "https://swapi.dev/api/people/schema"
-      ).then((response) => response.json());
-      setPeopleListSchema(peopleSchemaResult);
-      const peopleTableColumnHeadersResult = peopleSchemaResult.required.map(
-        (item) => {
-          return {
-            Header: item,
-            accessor: item,
-          };
-        }
-      );
-      setPeopleTableColumnHeaders(peopleTableColumnHeadersResult);
-    }
-    fetchPeopleList();
-    fetchPeopleListSchema();
-  }, []);
-
-  console.log(peopleList.results);
-  console.log(peopleListSchema.required);
-  console.log(peopleTableColumnHeaders);
-
+  const [peoplePage, setPeoplePage] = useState(1);
   const [toggleCharts, setToggleCharts] = useState("lineChart");
   const [loading, setLoading] = useState(false);
+
+  async function fetchPeopleList(x) {
+    let people = await fetch(`https://swapi.dev/api/people/?page=${x}`);
+    let peopleJSON = await people.json();
+    setLoading(false);
+    return peopleJSON;
+  }
+  async function fetchPeopleListSchema() {
+    const peopleSchemaResult = await fetch(
+      "https://swapi.dev/api/people/schema"
+    ).then((response) => response.json());
+    const peopleTableColumnHeadersResult = peopleSchemaResult.required.map(
+      (item) => {
+        return {
+          Header: item,
+          accessor: item,
+        };
+      }
+    );
+    return peopleTableColumnHeadersResult;
+  }
+  useEffect(() => {
+    fetchPeopleList(peoplePage).then((result) => setPeopleList(result));
+    fetchPeopleListSchema().then((result) =>
+      setPeopleTableColumnHeaders(result)
+    );
+  }, []);
+
+  console.log(peoplePage);
+  console.log(peopleList);
+  console.log(peopleList.results);
+  console.log(peopleTableColumnHeaders);
+
+  const handleNextPageClick = async () => {
+    if (peoplePage <= 8) {
+      setLoading(true);
+      const newPage = peoplePage + 1;
+      fetchPeopleList(newPage).then((result) => setPeopleList(result));
+      setPeoplePage(newPage);
+    }
+  };
+
+  const handleBackPageClick = () => {
+    if (peoplePage >= 2) {
+      const newPage = peoplePage - 1;
+      fetchPeopleList(newPage).then((result) => setPeopleList(result));
+      setPeoplePage(newPage);
+      setLoading(true);
+    }
+  };
 
   const handleHeaderClick = () => {
     if (toggleCharts === "lineChart") {
@@ -157,14 +178,28 @@ export function People() {
     >
       <Card
         heading="People"
-        style={{ maxWidth: "900px", ...spacing.sapUiContentPadding }}
+        style={{ maxWidth: "1200px", ...spacing.sapUiContentPadding }}
         avatar={<Icon name="table-view" />}
       >
         <AnalyticalTable
           data={peopleList.results}
           columns={peopleTableColumnHeaders}
-          visibleRows={5}
+          visibleRows={10}
+          scaleWidthMode={"Grow"}
+          loading={loading}
         />
+        <FlexBox
+          justifyContent={FlexBoxJustifyContent.Center}
+          wrap={FlexBoxWrap.Wrap}
+          style={spacing.sapUiContentPadding}
+        >
+          <Button icon="arrow-left" onClick={handleBackPageClick}>
+            Back
+          </Button>
+          <Button icon="arrow-right" onClick={handleNextPageClick}>
+            Next
+          </Button>
+        </FlexBox>
       </Card>
       <Card
         avatar={
