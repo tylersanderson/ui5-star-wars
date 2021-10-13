fa1291caa101ff86c41caa89577dc146b63f1dae
tylersanderson <tylersanderson@hotmail.com>
2021-10-12 19:44:52 -0500

    Fixed schema endpoint deprication issue

--- src/pages/people.component.jsx
+++ src/pages/people.component.jsx
@@ -73,23 +73,6 @@ export function People() {
       Header: "Gender",
       accessor: "gender",
     },
-    //   name: "name",
-    // height: "height",
-    // mass: "mass",
-    // hair_color: "hair_color",
-    // skin_color: "skin_color",
-    // eye_color: "eye_color",
-    // birth_year: "birth_year",
-    // gender: "gender",
-    // homeworld: "homeworld",
-    // films: "films",
-    // species: "species",
-    // vehicles: "vehicles",
-    // starships: "starships",
-    // created: "created",
-    // edited: "edited",
-    // url: "url,
-    //}
   ];
 
   useEffect(() => {
